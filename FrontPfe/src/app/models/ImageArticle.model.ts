fda281c2a8fd91e8779827dcfe30cb1b154ca5bb
shrsouahi <sahar.souahi@istic.ucar.tn>
2023-11-17 20:38:39 +0100

    trying with image upload

--- FrontPfe/src/app/models/ImageArticle.model.ts
+++ FrontPfe/src/app/models/ImageArticle.model.ts
@@ -1,12 +1,12 @@
 import { Article } from './Article.model';
 
 export class ImageArticle {
-  idImage: number;
+  id: number;
   url_image: string;
   article: Article;
 
-  constructor(idImage: number, url_image: string, article: Article) {
-    this.idImage = idImage;
+  constructor(id: number, url_image: string, article: Article) {
+    this.id = id;
     this.url_image = url_image;
     this.article = article;
   }
--- FrontPfe/src/app/pagesDashboard/add-article/add-article.component.ts
+++ FrontPfe/src/app/pagesDashboard/add-article/add-article.component.ts
@@ -7,6 +7,8 @@ import { ArticleService } from 'src/app/services/article.service';
 import { CategoryService } from 'src/app/services/category-service.service';
 import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { FileHandle } from 'src/app/models/file-handle.model';
+import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-add-article',
   templateUrl: './add-article.component.html',
@@ -15,14 +17,25 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 export class AddArticleComponent implements OnInit {
   articleForm: FormGroup;
   categories: Category[] = [];
-  article!: Article;
+  article: Article = {
+    bareCode: 0,
+    nomArticle: '',
+    descriptionArticle: '',
+    prixArticle: 0,
+    prixSolde: 0,
+    quantiteStock: 0,
+    categories: [],
+    images: [],
+    idArticle: 0,
+  };
 
   constructor(
     private fb: FormBuilder,
     private articleService: ArticleService,
     private categoryService: CategoryService,
     private router: Router,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private sanitizer: DomSanitizer
   ) {
     this.articleForm = this.fb.group({
       bareCode: ['', Validators.required],
@@ -68,6 +81,20 @@ export class AddArticleComponent implements OnInit {
           console.error('Error adding article:', error);
         }
       );
+    }
+  }
+
+  onFileSelected(event: any) {
+    if (event.target.files) {
+      const file = event.target.files[0];
+
+      const FileHandle: FileHandle = {
+        file: file,
+        url: this.sanitizer.bypassSecurityTrustUrl(
+          window.URL.createObjectURL(file)
+        ),
+      };
+      this.article.images.push(FileHandle);
     }
   }
 }
